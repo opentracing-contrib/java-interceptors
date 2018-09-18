1578b93d0e1073b9a08a53f98a61cba51b2a687a
Juraci Paixão Kröhling <juraci@kroehling.de>
2018-09-18 15:09:14 +0200

    Log exceptions in the span
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>

---
+++ src/test/java/io/opentracing/contrib/interceptors/application/ExceptionalEJB.java
@@ -0,0 +1,21 @@
+package io.opentracing.contrib.interceptors.application;
+
+import org.eclipse.microprofile.opentracing.Traced;
+
+import javax.ejb.Stateless;
+
+@Stateless
+@Traced
+public class ExceptionalEJB {
+    public void throwException() throws Exception {
+        throw new Exception("this is expected to happen");
+    }
+
+    public void throwRuntimeException() throws RuntimeException {
+        throw new RuntimeException("this is expected to happen");
+    }
+
+    public void throwError() throws Error {
+        throw new Error("this is expected to happen");
+    }
+}
